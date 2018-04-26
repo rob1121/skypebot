663015f090e6d8046979d7c973f935c80a585b14
Robinson L. Legaspi <robinson.legaspi@yahoo.com>
2018-04-26 17:58:17 +0800

    wip

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+const translate = require('translate');
+
+translate.engine = 'yandex';
+translate.key = 'trnsl.1.1.20180426T095042Z.23762791ca618ab3.009a970735a88ad1cded4b90f04cfcd728bdc9d3';
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+  console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+  appId: process.env.MicrosoftAppId,
+  appPassword: process.env.MicrosoftAppPassword
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+  translate(session.message.text, 'es').then(text => {
+    session.send("You said: %s", text);
+  });
+});
